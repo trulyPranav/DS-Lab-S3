faf652b44edafc2f639f29d26fc020c26c3fb927
Pranav M <pranavm265@gmail.com>
2024-10-23 00:43:37 +0530

    organization and bubbleSort

---
+++ Module 5/Sorting Techniques/bubbleSort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX 100
+
+int main(){
+    printf("\nEnter the size of the array: ");
+    int n;
+    scanf("%d", &n);
+    printf("\nEnter the Array to be sorted: ");
+    int arr[MAX];
+    for(int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+    
+    // Bubble Sorting:
+    int temp = 0;
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    // Printing the Sorted Array:
+    printf("\nSorted Array is: ");
+    for(int i=0; i<n; i++)
+        printf("%d ", arr[i]);
+}
